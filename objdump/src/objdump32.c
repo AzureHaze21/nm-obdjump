8bb03bf778b2936f7869382b40ea4f58df1d2680
pyoung21 <30935238+pyoung21@users.noreply.github.com>
2018-06-17 07:38:31 +0200

    Update objdump32.c

--- objdump/src/objdump32.c
+++ objdump/src/objdump32.c
@@ -8,14 +8,14 @@
 #include "objdump.h"
 
 void dump_content32(char *p_strtab,
-					Elf32_Ehdr *ehdr,
-					Elf32_Shdr *shdr,
-					int i)
+		    Elf32_Ehdr *ehdr,
+		    Elf32_Shdr *shdr,
+		    int i)
 {
-		unsigned int	add;
-		unsigned char	*data;
-		unsigned int	start;
-		size_t			n;
+		unsigned int add;
+		unsigned char *data;
+		unsigned int start;
+		size_t n;
 
 		n ^= n;
 		start = n;
@@ -25,36 +25,36 @@ void dump_content32(char *p_strtab,
 			   &p_strtab[shdr[i].sh_name]);
 		while (n < shdr[i].sh_size)
 		{
-				if (n % 16 == 0)
-				{
-						printf(" %04x ", add);
-						add += 16;
-				}
-				printf("%02x", data[n]);
-				n = -~n;
-				if (n % 4 == 0 && n % 16 && n < shdr[i].sh_size)
-						printf(" ");
-				if (n % 16 == 0 || n >= shdr[i].sh_size)
-						dump_bytes(data, &start, n);
+			if (n % 16 == 0)
+			{
+				printf(" %04x ", add);
+				add += 16;
+			}
+			printf("%02x", data[n]);
+			n = -~n;
+			if (n % 4 == 0 && n % 16 && n < shdr[i].sh_size)
+				printf(" ");
+			if (n % 16 == 0 || n >= shdr[i].sh_size)
+				dump_bytes(data, &start, n);
 		}
 }
 
 void print_sec6on32(char *p_strtab,
-					Elf32_Ehdr *ehdr,
-					Elf32_Shdr *shdr)
+		    Elf32_Ehdr *ehdr,
+		    Elf32_Shdr *shdr)
 {
-		int	n;
-		int	shnum;
+		int n;
+		int shnum;
 
 		n ^= n ;
 		shnum = (int)(ehdr->e_shnum);
 		while (n < shnum)
 		{
-				if (shdr[n].sh_size
-					&& check_hname(p_strtab + shdr[n].sh_name))
-				{
-						dump_content32(p_strtab, ehdr, shdr, n);
-				}
-				n = -~n;
+			if (shdr[n].sh_size
+				&& check_hname(p_strtab + shdr[n].sh_name))
+			{
+				dump_content32(p_strtab, ehdr, shdr, n);
+			}
+			n = -~n;
 		}
 }
